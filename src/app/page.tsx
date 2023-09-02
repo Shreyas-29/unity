4ab008bb18a4a05e182fe03f8289c770c4fec61a
29Shreyas <sihasaneshreyas@gmail.com>
2023-09-03 01:48:51 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { bookmark, deletePost, getCurrentUser, getPosts, getStories, like } from "@/actions";
-import { Followbar, GeneralFeed, Loader, Sidebar } from "@/components";
+import { Followbar, GeneralFeed, Sidebar } from "@/components";
 import { getAuthSession } from "@/lib/auth";
 import { ExtendedPost } from "@/types/post";
 
@@ -13,47 +13,19 @@ export default async function FeedPage() {
 
     const stories = await getStories();
 
-    // Ok, now the current user id is getting added into the post author followers ids, but now how do i get the id's of the other users which the current logged in user is following
-
-    // const createPost = async (postData: FormData) => {
-    //     "use server";
-
-    //     const content = postData.get("content");
-    //     const location = postData.get("location");
-    //     const image = postData.get("image");
-    //     const audience = postData.get("audience");
-
-    //     try {
-    //         await db.post.create({
-    //             data: {
-    //                 content: content as string,
-    //                 location: location as string,
-    //                 image: image as string,
-    //                 audience: "PUBLIC",
-    //                 authorId: session?.user?.id as string,
-    //             }
-    //         });
-    //     } catch (error) {
-    //         console.log("Error creating post", error);
-    //     }
-
-    //     revalidatePath("/");
-    // };
-
     return (
-        // <div className="relative flex w-full max-w-lg min-h-screen py-16 mx-auto overflow-scroll">
-        //     {/* <Sidebar session={session} />
-        //     <GeneralFeed
-        //         session={session}
-        //         user={user}
-        //         posts={posts as ExtendedPost[]}
-        //         like={like}
-        //         bookmark={bookmark}
-        //         stories={stories}
-        //         deletePost={deletePost}
-        //     />
-        //     <Followbar /> */}
-        // </div>
-        <Loader />
+        <div className="relative flex w-full max-w-lg min-h-screen py-16 mx-auto overflow-scroll">
+            <Sidebar session={session} />
+            <GeneralFeed
+                session={session}
+                user={user}
+                posts={posts as ExtendedPost[]}
+                like={like}
+                bookmark={bookmark}
+                stories={stories}
+                deletePost={deletePost}
+            />
+            <Followbar />
+        </div>
     )
 }
